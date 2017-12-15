49a9fed1eef937bfe245e444193baf1c02cf94aa
Lushui <718093910@qq.com>
2017-12-15 15:04:11 +0800

    update config

--- Apps/Common/Conf/config.php
+++ Apps/Common/Conf/config.php
@@ -2,10 +2,10 @@
 return array(
 	//'配置项'=>'配置值'
 	'DB_TYPE'               =>  'mysql',     // 数据库类型
-    'DB_HOST'               =>  '', // 服务器地址
-    'DB_NAME'               =>  '',          // 数据库名
-    'DB_USER'               =>  '',      // 用户名
-    'DB_PWD'                =>  '',          // 密码
+    'DB_HOST'               =>  '119.28.111.247', // 服务器地址
+    'DB_NAME'               =>  'note',          // 数据库名
+    'DB_USER'               =>  'note',      // 用户名
+    'DB_PWD'                =>  '8Z2MzKYaLx',          // 密码
     'DB_PORT'               =>  '3306',        // 端口
     'DB_PREFIX'             =>  '',    // 数据库表前缀
 );
